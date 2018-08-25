1f1b5ccd01bba056170ff380c9d8ac71b78524df
DEV3L <jus.beall@gmail.com>
2018-08-24 23:28:51 -0400

    Optimize search using hash lookup
    Refactor performance test harness

--- search-optimization/src/index.js
+++ search-optimization/src/index.js
@@ -1,43 +1,36 @@
 const Search = require("./search");
 
-const testIterations = 100;
+const testIterations = 1000;
+
+function testPerformance(searchId) {
+  const totalStart = new Date();
+
+  const items = [];
+
+  for (let i = 1; i <= searchId; i++) {
+    items.push({id: i})
+  }
+
+  const searchSmall = new Search(items);
+
+  const timings = [];
+  for (let i = 0; i < testIterations; i++) {
+    const start = new Date();
+    searchSmall.find(searchId);
+    const stop = new Date();
+    timings.push(stop - start);
+  }
+
+  const totalStop = new Date();
+
+  const timingsSum = timings.reduce((total, current) => total + current, 0);
+
+  console.log(`Average time for find for ${searchId} records: ${timingsSum / testIterations} ms`);
+  console.log(`Total time for find for ${searchId} records: ${totalStop - totalStart} ms`);
+}
 
 const smallId = 3;
 const largeId = 1000000;
 
-// small data set
-const itemsSmall = [];
-for (let i = 1; i <= smallId; i++) {
-  itemsSmall.push({id: i})
-}
-const searchSmall = new Search(itemsSmall);
-
-const timingsSmall = [];
-for (let i = 0; i < testIterations; i++) {
-  const start = new Date();
-  searchSmall.find(smallId);
-  const stop = new Date();
-  timingsSmall.push(stop - start);
-}
-
-const timingsSumSmall = timingsSmall.reduce((total, current) => total + current, 0);
-console.log(`Average time for small search: ${timingsSumSmall / testIterations}`);
-
-
-// large data set
-const itemsLarge = [];
-for (let i = 1; i <= largeId; i++) {
-  itemsLarge.push({id: i})
-}
-const searchLarge = new Search(itemsLarge);
-
-const timingsLarge = [];
-for (let i = 1; i < testIterations; i++) {
-  const start = new Date();
-  searchLarge.find(largeId);
-  const stop = new Date();
-  timingsLarge.push(stop - start);
-}
-
-const largeTimingsSum = timingsLarge.reduce((total, current) => total + current, 0);
-console.log(`Average time for large search: ${largeTimingsSum / testIterations}`);
+testPerformance(smallId);
+testPerformance(largeId);
--- search-optimization/src/search.js
+++ search-optimization/src/search.js
@@ -1,15 +1,29 @@
 class Search {
   constructor(people) {
-    this.people = people;
+    // this.people = people;
+    const peopleMap = [];
+    people.forEach(person => peopleMap[person.id] = person);
+    this.people = peopleMap
   }
 
+  // hash map optimized
   find(id) {
-    for (let person of this.people) {
-      if (person.id === id) {
-        return person;
-      }
-    }
+    return this.people[id]
   }
+
+  // // declarative
+  // find(id) {
+  //   return this.people.find(person => person.id === id);
+  // }
+  //
+  // // imperative
+  // find(id) {
+  //   for (let person of this.people) {
+  //     if (person.id === id) {
+  //       return person;
+  //     }
+  //   }
+  // }
 }
 
 module.exports = Search;
