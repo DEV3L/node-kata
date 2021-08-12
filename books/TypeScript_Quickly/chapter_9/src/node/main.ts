ddd138070a88d4f8ed5cd6cd23248c9abf274abb
Justin Beall <jus.beall@gmail.com>
2021-08-12 00:17:09 -0400

    TSQ-10: Add websocket blockchain example

--- books/TypeScript_Quickly/chapter_9/src/node/main.ts
+++ books/TypeScript_Quickly/chapter_9/src/node/main.ts
@@ -3,18 +3,18 @@ import { Blockchain } from '../lib/bc_transactions';
 (async function main(): Promise<void> {
   console.log('⏳ Initializing the blockchain, creating the genesis block...');
 
-  const bc = new Blockchain();
-  await bc.createGenesisBlock();
+  const blockChain = new Blockchain();
+  await blockChain.createGenesisBlock();
 
-  bc.createTransaction({ sender: 'John', recipient: 'Kate', amount: 50 });
-  bc.createTransaction({ sender: 'Kate', recipient: 'Mike', amount: 10 });
+  blockChain.createTransaction({ sender: 'John', recipient: 'Kate', amount: 50 });
+  blockChain.createTransaction({ sender: 'Kate', recipient: 'Mike', amount: 10 });
 
-  await bc.minePendingTransactions();
+  await blockChain.minePendingTransactions();
 
-  bc.createTransaction({ sender: 'Alex', recipient: 'Rosa', amount: 15 });
-  bc.createTransaction({ sender: 'Gina', recipient: 'Rick', amount: 60 });
+  blockChain.createTransaction({ sender: 'Alex', recipient: 'Rosa', amount: 15 });
+  blockChain.createTransaction({ sender: 'Gina', recipient: 'Rick', amount: 60 });
 
-  await bc.minePendingTransactions();
+  await blockChain.minePendingTransactions();
 
-  console.log(JSON.stringify(bc, null, 2));
+  console.log(JSON.stringify(blockChain, null, 2));
 })();
